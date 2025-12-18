d6e80abed7d5add777290964e51eeb98075c8724
bala-amypo <bala@amypo.in>
2025-12-18 16:00:44 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -38,7 +38,23 @@ public class DeviceCatalogItem{
         return deviceCode;
     }
     public void setDeviceCode(String deviceCode){
-        
+        this.deviceCode=deviceCode;
     }
+    public String getDeviceType(){
+        return deviceType;
+    }
+    public void setDeviceType(String deviceType){
+        this.deviceType=deviceType;
+    }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model=model;
+    }
+    public Integer getMaxAllowedPerEmployee(){
+        return maxAllowedPerEmployee;
+    }
+    public void setMaxAllowedPerEmployee(Integer)
 
 }
