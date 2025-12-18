df3871286856cf76bd497f2d45d21739ed6aae7a
bala-amypo <bala@amypo.in>
2025-12-18 16:57:44 +0000

    User commit

--- src/main/java/com/example/demo/model/EligibilityCheckRecord.java
+++ src/main/java/com/example/demo/model/EligibilityCheckRecord.java
@@ -11,5 +11,63 @@ public class EligibilityCheckRecord{
     private Long deviceItemId;
     private Boolean isEligible;
     private LocalDateTime checkedAt;
+
+    public EligibilityCheckRecord(){
+
+    }
+    public EligibilityCheckRecord(private Long id,
     
+     Long employeeId,
+     Long deviceItemId,
+     Boolean isEligible,
+     LocalDateTime checkedAt)
+
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Long getEmployeeId() {
+    return employeeId;
+}
+
+public void setEmployeeId(Long employeeId) {
+    this.employeeId = employeeId;
+}
+
+public Long getDeviceItemId() {
+    return deviceItemId;
+}
+
+public void setDeviceItemId(Long deviceItemId) {
+    this.deviceItemId = deviceItemId;
+}
+
+public Boolean getIsEligible() {
+    return isEligible;
+}
+
+public void setIsEligible(Boolean isEligible) {
+    this.isEligible = isEligible;
+}
+
+public String getReason() {
+    return reason;
+}
+
+public void setReason(String reason) {
+    this.reason = reason;
+}
+
+public LocalDateTime getCheckedAt() {
+    return checkedAt;
+}
+
+public void setCheckedAt(LocalDateTime checkedAt) {
+    this.checkedAt = checkedAt;
+}
+
 }
