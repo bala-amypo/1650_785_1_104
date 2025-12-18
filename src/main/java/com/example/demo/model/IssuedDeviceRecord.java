b5c600811b3f342a8a4649cca752b0caf915fe8b
bala-amypo <bala@amypo.in>
2025-12-18 16:12:44 +0000

    User commit

--- src/main/java/com/example/demo/model/IssuedDeviceRecord.java
+++ src/main/java/com/example/demo/model/IssuedDeviceRecord.java
@@ -15,12 +15,47 @@ public class IssuedDeviceRecord{
     public IssuedDeviceRecord(){
 
     }
-    public IssuedDeviceRecord(Long id,Long employeeId,Long deviceItemId,LocalDate issuedDate,LocalDate returnedDate){
+    public IssuedDeviceRecord(Long id,Long employeeId,Long deviceItemId,LocalDate issuedDate,LocalDate returnedDate,String status){
         this.employeeId=employeeId;
         this.deviceItemId=deviceItemId;
         this.issuedDate=issuedDate;
-        this.returnedDate=retuenedDate;
-       
-        this.createdAt=createdAt;
+        this.returnedDate=returnedDate;
+        this.status=status;
+    }
+    ublic Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public Long getEmployeeId(){
+        return employeeId;
+    }
+    public void setEmployeeId(Long employeeId){
+        this.employeeId=employeeId;
+    }
+    public Long deviceItemId(){
+        return deviceItemId;
+    }
+    public void setfullname(String fullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department){
+        this.department=department;
+    }
+    public String getJobRole(){
+        return jobRole;
+    }
+    public void setJobRole(String jobRole){
+        this.jobRole=jobRole;
     }
 }
