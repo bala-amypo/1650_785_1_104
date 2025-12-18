becd5b88c4b98e55b60199a2003e047489cffa6e
bala-amypo <bala@amypo.in>
2025-12-18 16:09:45 +0000

    User commit

--- src/main/java/com/example/demo/model/IssuedDeviceRecord.java
+++ src/main/java/com/example/demo/model/IssuedDeviceRecord.java
@@ -12,5 +12,15 @@ public class IssuedDeviceRecord{
     private LocalDate issuedDate;
     private LocalDate returnedDate;
     private String status;
-    
+    public IssuedDeviceRecord(){
+
+    }
+    public IssuedDeviceRecord(Long id,Long employeeId,Long deviceItemId,LocalDate issuedDate,LocalDate returnedDate){
+        this.employeeId=employeeId;
+        this.deviceItemId=deviceItemId;
+        this.issuedDate=issuedDate;
+        this.returnedDate=retuenedDate;
+       
+        this.createdAt=createdAt;
+    }
 }
