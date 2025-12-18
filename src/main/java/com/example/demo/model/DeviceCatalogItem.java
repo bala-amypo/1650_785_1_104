be69c4f6f5c731b8c18ff77988910ee2de98d2d8
bala-amypo <bala@amypo.in>
2025-12-18 15:45:47 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -1,7 +1,15 @@
 package com.example.demo.model;
+import jakarta.persistence.*;
 
 @Entity
 public class DeviceCatalogItem{
     @Id
-    
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true)
+    private String deviceCode;
+    private String deviceType;
+    private String model;
+    private Integer maxAllowedPerEmployee;
+    private Boolean 
 }
