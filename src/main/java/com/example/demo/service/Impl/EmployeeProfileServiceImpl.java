251ccdc2974ebf210dc213eb5adcaa4b1a5419a8
bala-amypo <bala@amypo.in>
2025-12-20 18:13:34 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/EmployeeProfileServiceImpl.java
@@ -1,10 +1,8 @@
-
 package com.example.demo.service.impl;
 
 import java.util.List;
-import com.example.demo.exception.*;
-import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
+import com.example.demo.model.EmployeeProfile;
 import com.example.demo.service.EmployeeProfileService;
 
 public class EmployeeProfileServiceImpl implements EmployeeProfileService {
@@ -15,30 +13,11 @@ public class EmployeeProfileServiceImpl implements EmployeeProfileService {
         this.repo = repo;
     }
 
-    public EmployeeProfile createEmployee(EmployeeProfile emp) {
-
-        if (repo.findByEmployeeId(emp.getEmployeeId()).isPresent()) {
-            throw new BadRequestException("EmployeeId already exists");
-        }
-        if (repo.findByEmail(emp.getEmail()).isPresent()) {
-            throw new BadRequestException("Email already exists");
-        }
-        return repo.save(emp);
+    public EmployeeProfile save(EmployeeProfile e) {
+        return repo.save(e);
     }
 
-    public EmployeeProfile getEmployeeById(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
-    }
-
-    public List<EmployeeProfile> getAllEmployees() {
+    public List<EmployeeProfile> findAll() {
         return repo.findAll();
     }
-
-    public EmployeeProfile updateEmployeeStatus(Long id, boolean active) {
-        EmployeeProfile emp = getEmployeeById(id);
-        emp.setActive(active);
-        return repo.save(emp);
-    }
 }
-
--- src/main/java/com/example/demo/service/PolicyRuleService.java
+++ src/main/java/com/example/demo/service/PolicyRuleService.java
@@ -1,3 +1,8 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.model.PolicyRule;
+
 public interface PolicyRuleService {
     PolicyRule save(PolicyRule p);
     List<PolicyRule> findAll();
