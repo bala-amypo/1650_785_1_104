27a0fc4646c9aafe7f6c369253c6b913da728794
bala-amypo <bala@amypo.in>
2025-12-21 11:49:39 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeProfile.java
+++ src/main/java/com/example/demo/model/EmployeeProfile.java
@@ -1,6 +1,8 @@
+
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class EmployeeProfile {
@@ -9,25 +11,34 @@ public class EmployeeProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(unique = true)
+    private String employeeId;
+
+    private String fullName;
+    private String email;
     private String department;
-    private String role;
+    private String jobRole;
     private Boolean active;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
 
     public EmployeeProfile() {}
 
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
+    public String getEmployeeId() { return employeeId; }
+    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public String getDepartment() { return department; }
     public void setDepartment(String department) { this.department = department; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
-
+    public String getJobRole() { return jobRole; }
+    public void setJobRole(String jobRole) { this.jobRole = jobRole; }
     public Boolean getActive() { return active; }
     public void setActive(Boolean active) { this.active = active; }
 }
