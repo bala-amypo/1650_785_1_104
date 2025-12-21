45bb435bd27c076cbb95c8c231acd6ff4d7da5b2
bala-amypo <bala@amypo.in>
2025-12-21 12:41:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityCheckRecordRepository.java
@@ -2,12 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EligibilityCheckRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import java.util.List;  // ✅ THIS IS REQUIRED
 
 public interface EligibilityCheckRecordRepository
         extends JpaRepository<EligibilityCheckRecord, Long> {
-// List<EligibilityCheckRecord> records = repo.findByEmployeeId(employeeId);
-// return records;
 
     List<EligibilityCheckRecord> findByEmployeeId(Long employeeId);
 }
