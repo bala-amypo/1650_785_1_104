1909e63b977d6e58887dd13e7269531e0a8b8b56
bala-amypo <bala@amypo.in>
2025-12-22 08:23:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceCatalogController.java
+++ src/main/java/com/example/demo/controller/DeviceCatalogController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.DeviceCatalogItem;
+import com.example.demo.model.DeviceCatalogItem;
 import com.example.demo.service.DeviceCatalogService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
