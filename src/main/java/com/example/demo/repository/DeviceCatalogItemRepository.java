d84a3b71aa7c073f69eb7b18a04b77ce5fdbbb9b
bala-amypo <bala@amypo.in>
2025-12-22 18:14:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
+++ src/main/java/com/example/demo/repository/DeviceCatalogItemRepository.java
@@ -1,4 +1,5 @@
 package com.example.demo.repository;
+import java.util.Optional;
 
 import com.example.demo.model.DeviceCatalogItem;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/example/demo/repository/IssuedDeviceRecordRepository.java
+++ src/main/java/com/example/demo/repository/IssuedDeviceRecordRepository.java
@@ -1,4 +1,5 @@
 package com.example.demo.repository;
+import java.util.Optional;
 
 import com.example.demo.model.IssuedDeviceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
