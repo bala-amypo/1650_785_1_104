2d7956ec1e31fda58d64deae160507b02f62aa3c
bala-amypo <bala@amypo.in>
2025-12-25 13:53:58 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/EligibilityCheckServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/EligibilityCheckServiceImpl.java
@@ -62,16 +62,16 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.model.EligibilityCheckRecord;
-import com.example.demo.repository.EligibilityCheckRepository;
+import com.example.demo.repository.EligibilityCheckRecordRepository;
 import com.example.demo.service.EligibilityCheckService;
 import org.springframework.stereotype.Service;
 
 @Service   // ⭐ REQUIRED
 public class EligibilityCheckServiceImpl implements EligibilityCheckService {
 
-    private final EligibilityCheckRepository repository;
+    private final EligibilityCheckRecordRepository repository;
 
-    public EligibilityCheckServiceImpl(EligibilityCheckRepository repository) {
+    public EligibilityCheckServiceImpl(EligibilityCheckRecordRepository repository) {
         this.repository = repository;
     }
 
